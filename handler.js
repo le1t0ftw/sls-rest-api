41832baac56e11fea012b758e21dc97eee9bafd4
Leonel De Vera <Leonel.De.Vera.-ND@disney.com>
2023-03-06 21:11:56 -0300

    initial commit

---
+++ handler.js
@@ -0,0 +1,31 @@
+'use strict';
+
+module.exports.createNote = async (event) => {
+  return {
+    statusCode: 201,
+    body: JSON.stringify("A new note Created!")
+  };
+};
+
+module.exports.updateNote = async (event) => {
+  let notesId = event.pathParameters.id
+  return {
+    statusCode: 200,
+    body: JSON.stringify("The note with " + notesId + "has been updated!")
+  };
+};
+
+module.exports.deleteNote = async (event) => {
+  let notesId = event.pathParameters.id
+  return {
+    statusCode: 200,
+    body: JSON.stringify("The note with " + notesId + "has been deleted!")
+  };
+};
+
+module.exports.getAllNotes = async (event) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify("All notes are returned!")
+  };
+};
